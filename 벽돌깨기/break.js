70726ac0de41e283939feb72a10f7e3521f30227
hyoseon0319 <hyosun0319@gmail.com>
2020-03-05 23:25:37 +0900

    03-05 허효선 마무리

--- 벽돌깨기/break.js
+++ 벽돌깨기/break.js
@@ -19,7 +19,9 @@ var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 var score = 0;
+var lives = 3;
 
+// 벽돌을 배열에 담음
 var bricks = [];
 for (var c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
@@ -29,15 +31,17 @@ for (var c = 0; c < brickColumnCount; c++) {
     }
 }
 
+
+// 이벤트 리스너 등록
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.addEventListener("mousemove", mouseMoveHandler, false);
 
 function keyDownHandler(e) { // 키를 누르면
-    if (e.keyCode == 39) { // 오른쪽 방향키
+    if (e.keyCode == 39) { // 오른쪽 방향키 39
         rightPressed = true;
     }
-    else if (e.keyCode == 37) { // 왼쪽 방향키
+    else if (e.keyCode == 37) { // 왼쪽 방향키 37
         leftPressed = true;
     }
 }
@@ -52,18 +56,19 @@ function keyUpHandler(e) { // 키에서 손을 떼면
 }
 
 
-function mouseMoveHandler(e) {
+function mouseMoveHandler(e) { // 마우스로 패들 움직임
     var relativeX = e.clientX - canvas.offsetLeft;
-    if(relativeX > 0 && relativeX < canvas.width) {
-        paddleX = relativeX - paddleWidth/2;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
     }
 }
 
-function collisionDetection() { // 벽돌 충돌 감지 함수
+function collisionDetection() { // 벽돌과 공의 충돌 감지
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
             var b = bricks[c][r];
             // 충돌 감지 반복에서 사용할 벽돌 객체를 b에 저장
+
             if (b.status == 1) { // 충돌이 일어났는지 확인
                 if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) { // 충돌이 발생했다면
                     // 공의 x 좌표 > 벽돌의 x 좌표
@@ -73,20 +78,30 @@ function collisionDetection() { // 벽돌 충돌 감지 함수
                     dy = -dy;
                     b.status = 0; // 다시 그리지 않게 벽돌의 속성 0으로
                     score++;
+                    if (score == brickRowCount * brickColumnCount) {
+                        alert("미션 성공!");
+                        document.location.reload();
+                    }
                 }
             }
         }
     }
 }
 
-function drawScore(){
+function drawScore() { // 게임점수 기록
     ctx.font = "20px jua";
     ctx.loc = "center";
     ctx.fillStyle = "#f6ea8c";
-    ctx.fillText("Score: "+score, 8, 20);
+    ctx.fillText("Score: " + score, 8, 20);
 }
 
-function drawBall() { // 공을 그리는 코드
+function drawLives() { // 생명점수 기록
+    ctx.font = "20px Jua";
+    ctx.fillStyle = "#eb9f9f";
+    ctx.fillText("Lives : " + lives, canvas.width - 80, 20);
+}
+
+function drawBall() { // 공 그리기
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     // 원의 반지름 값을 대입함
@@ -96,7 +111,7 @@ function drawBall() { // 공을 그리는 코드
 
 }
 
-function drawPaddle() { // 공을 치기 위한 paddle 정의
+function drawPaddle() { // 패들 그리기
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "#f6ea8c";
@@ -105,7 +120,7 @@ function drawPaddle() { // 공을 치기 위한 paddle 정의
 }
 
 
-function drawBricks() { // 벽돌 생성 함수
+function drawBricks() { // 벽돌 생성
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
             if (bricks[c][r].status == 1) { // status =1 , 벽돌을 그림
@@ -123,7 +138,8 @@ function drawBricks() { // 벽돌 생성 함수
     }
 }
 
-function draw() {    // 공을 움직이는 코드
+function draw() { // 공을 움직이는 코드
+    
     // 캔버스 내용들을 지워주기 위한 메소드
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // 앞의 0,0 - 직사각형의 좌상단 모서리 표시할 x,y좌표
@@ -131,6 +147,7 @@ function draw() {    // 공을 움직이는 코드
     drawBall();
     drawPaddle();
     drawScore();
+    drawLives();
     collisionDetection(); // 충돌 감지 활성화
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -138,14 +155,24 @@ function draw() {    // 공을 움직이는 코드
     if (y + dy < ballRadius) { // 밑면에 충돌하는 순간
         dy = -dy;
     }
-    else if (y + dy > canvas.height - ballRadius) {
-        // 공과 패들 사이의 충돌 감지
+    else if (y + dy > canvas.height - ballRadius) { // 패들에 충돌했는지 아래 벽에 닿으면 오버
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
         }
-        else {
-            // alert("GAME OVER !");
-            document.location.reload();
+        else { // 플레이어에게 목숨 점수 주기
+            lives--;
+            if (!lives) {
+                alert("GAME OVER !");
+                document.location.reload();
+                clearInterval(interval);
+            }
+            else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 2;
+                dy = -2;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
         }
     }
 
@@ -160,8 +187,11 @@ function draw() {    // 공을 움직이는 코드
 
     x += dx;
     y += dy;
-
+    requestAnimationFrame(draw);
+    // 사용자 기기의 화면 주사율에 맞게 계속적인 실행 반복
+    // interval 이용보다 효율적!
+    clearInterval(interval);
 }
-setInterval(draw, 10); // 10밀리초마다 실행
 
+draw();
 
